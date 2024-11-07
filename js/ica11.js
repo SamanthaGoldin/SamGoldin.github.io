4c79ad068a5eb11d5743c1bdc1169a6cb3aa1fb7
SamanthaGoldin <sago8644@colorado.edu>
2024-11-06 19:39:18 -0700

    ica 11

--- js/ica11.js
+++ js/ica11.js
@@ -1,12 +1,24 @@
 
-console.log (tellFortune("2","Casey", "location", "jobTitle"));
+        
+   function tellFortune(children, partner, location, job) {
+    const fortune = "You will be a " + job + " in " + location + ", and married to " + partner + " with " + children + " kids.";
+    document.getElementById('fortuneOutput').innerHTML += "<p>" + fortune + "</p>";
+  }
 
-function tellFortune (numChildren, partnerName, location, jobTitle){
-  
-console.log (numChildren, partnerName, location, jobTitle)
+  function calculateDogAge(puppyAge) {
+    const dogAge = puppyAge * 7;
+    const dogAgeMessage = "Your doggie is " + dogAge + " years old in dog years!";
+    document.getElementById('dogAgeOutput').innerHTML += "<p>" + dogAgeMessage + "</p>";
+  }
 
-let numChildren = "";
+  // Calling tellFortune 3 times
+  tellFortune(2, 'Bella', 'LA', 'Teacher');
+  tellFortune(3, 'Sarah', 'London', 'Writer');
+  tellFortune(1, 'Taylor', 'Colorado', 'Journalist');
 
-}
+  // Calling calculateDogAge 3 times
+  calculateDogAge(1);
+  calculateDogAge(3);
+  calculateDogAge(5);
 
 
