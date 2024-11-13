f313346197c2256cc1d7f115fdbf142533d034ab
SamanthaGoldin <sago8644@colorado.edu>
2024-11-13 12:42:12 -0700

    ica12.html

---
+++ js/ica12.js
@@ -0,0 +1,28 @@
+const apiEndpoint = 'https://v2.jokeapi.dev/joke/Any?';
+function getJoke() {
+    fetch(apiEndpoint)
+        .then(res => {
+            if (res.ok) {
+                throw new Error('Failed to fetch the joke');
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log('Fetched joke:', data.setup + " - " + data.punchline);
+            displayRes(data.setup + " - " + data.punchline);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Sorry, there was an error fetching the joke.');
+        });
+}
+function displayRes(jokeText) {
+    document.getElementById('output').textContent = jokeText;
+}
+
+document.getElementById('fetchJokeBtn').addEventListener('click', () => {
+    console.log('Button clicked');
+    getJoke();
+});
+
+getJoke();
---
+++ js/wk_12.js
@@ -0,0 +1,26 @@
+
+
+
+console.log (ourObject);
+
+
+let  myData ={};
+
+fetch{'https://catfact.ninja/fact'}
+.then(res => 
+
+if(res.ok){
+console.log(res.json());
+myData=res;
+myData[fact]
+
+}else{
+
+    console.log (res);
+}
+
+.catch(error => {console.log(error)})
+
+
+fetchData();
+document.getElementById("generate").addEventListenter("click", e=> {fetchData();})
